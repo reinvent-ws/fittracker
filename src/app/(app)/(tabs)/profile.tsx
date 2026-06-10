d6c7227e2170c7f9ff28bf88518ba08a70dbae34
reinvent-ws <reinventws@gmail.com>
2026-06-10 03:50:53 -0300

    Update: add a button for sign out

--- src/app/(app)/(tabs)/profile.tsx
+++ src/app/(app)/(tabs)/profile.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import { Text } from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context'
+import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Profile() {
+export default function ProfilePage() {
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    Alert.alert("Sign Out", "Are you sure you want to sign out?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Sign Out",
+        style: "destructive",
+        onPress: () => signOut(),
+      },
+    ]);
+  };
+
   return (
-    <SafeAreaView className='flexa-1 px-4'>
-        <Text>Profile</Text>
+    <SafeAreaView className="flexa-1 px-4">
+      <Text>Profile</Text>
+
+      {/* Sign Out */}
+      <View className="px-6 mb-8">
+        <TouchableOpacity
+          onPress={handleSignOut}
+          className="bg-red-600 rounded-2xl p-4 shadow-sm"
+          activeOpacity={0.8}
+        >
+          <View className="flex-row items-center justify-center">
+            <Ionicons name="log-out-outline" size={20} color="white" />
+            <Text className="text-white font-semibold text-lg ml-2">
+              Sign Out
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
