921d7b9df2b6b75a58f4d4ce0cb1694b7914eef6
reinvent-ws <reinventws@gmail.com>
2026-06-12 03:07:57 -0300

    Set token and productId in configuration, pull extract typegen and set sanity´s configuration

---
+++ src/app/components/ExerciseCard.tsx
@@ -0,0 +1,68 @@
+import { Exercise } from "@/lib/sanity/types";
+import { View, Text, TouchableOpacity } from "react-native";
+
+// const getDifficultyColor = (difficulty: string) => {
+//   switch (difficulty) {
+//     case "beginner":
+//       return "bg-green-500";
+//     case "intermediate":
+//       return "bg-yellow-500";
+//     case "advanced":
+//       return "bg-red-500";
+//     default:
+//       return "bg-gray-500";
+//   }
+// };
+
+// const getDifficultyText = (difficulty: string) => {
+//   switch (difficulty) {
+//     case "beginner":
+//       return "Beginner";
+//     case "intermediate":
+//       return "Intermediate";
+//     case "advanced":
+//       return "Advanced";
+//     default:
+//       return "Unknown";
+//   }
+// };
+
+const getDifficulty = (
+  difficulty: string,
+): { color: string; title: string } => {
+  switch (difficulty) {
+    case "beginner":
+      return { color: "bg-green-500", title: "Beginner" };
+    case "intermediate":
+      return { color: "bg-yellow-500", title: "Intermediate" };
+    case "advanced":
+      return { color: "bg-red-500", title: "Advanced" };
+    default:
+      return { color: "bg-gray-500", title: "Unknown" };
+  }
+};
+
+type ExerciseCardProps = {
+  item: Exercise;
+  onPress: () => void;
+  showChevron?: boolean;
+};
+
+export default function ExerciseCard({
+  item,
+  onPress,
+  showChevron = false,
+}: ExerciseCardProps) {
+  return (
+    <TouchableOpacity
+      key={item._id}
+      className="bg-white rounded-2xl mb-4 shadow-sm border border-gray-100"
+    >
+      <View className="text-black">
+        <Text>Exercise Card</Text>
+        <Text>{item.name}</Text>
+        <Text>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
---
+++ src/lib/sanity/client.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+// Client safe config
+const config = {
+  projectId: "y0vrawvs",
+  dataset: "production",
+  apiVersion: "2026-05-31",
+  useCdn: false,
+};
+
+export const client = createClient(config);
+
+const adminConfig = {
+  ...config,
+  token: process.env.SANITY_API_TOKEN,
+};
+
+export const adminClient = createClient(adminConfig);
+
+// Image URL builder
+const builder = imageUrlBuilder(config);
+export const urlFor = (source: string) => builder.image(source);
