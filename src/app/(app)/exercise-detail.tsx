b14b0fa5db1654b4e71eebb9f2ee1e8f07b760bb
reinvent-ws <reinventws@gmail.com>
2026-06-13 02:45:52 -0300

    add a nem component for exercise detail

---
+++ src/app/(app)/exercise-detail.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ExerciseDetail() {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Text>ExerciseDetail</Text>
+    </SafeAreaView>
+  );
+}
